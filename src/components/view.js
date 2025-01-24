38339c6e611fb27946a4e6cc2a0638c5de3fc818
kevin <kevinhui98@gmail.com>
2025-01-24 17:19:29 -0500

    adding the scroll btn for foryou page

--- src/components/view.js
+++ src/components/view.js
@@ -5,7 +5,10 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import SmsIcon from '@mui/icons-material/Sms';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import ReplyIcon from '@mui/icons-material/Reply';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import { React, useState } from 'react'
+import { grey } from '@mui/material/colors';
 function Video() {
     return (
         <video
@@ -50,61 +53,72 @@ const View = () => {
     const [shareClick, SetShareClick] = useState(false)
 
     return (
-        <Stack direction='row' width={'80vw'} gap={2} sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Video />
-            <Stack direction={'column'} justifyContent={'end'} mb={1} >
-                <Box mb={2} ml={2.68} sx={{ display: 'flex', 'flex-direction': 'column', justifyContent: 'center', height: '40px', width: '40px', transform: 'scale(1.5)' }}>
-                    <UserButton />
-                </Box>
-                {action.map((btn, idx) => (
-                    <Button key={idx} width={'10px'} height={'200px'} sx={{ display: 'flex', 'flex-direction': 'column', justifyContent: 'center' }} onClick={() => {
-                        if (btn.idx == 0) {
-                            btn.clicked = !btn.clicked
-                            btn.clicked ? SetLikeClick(btn.count++) : SetLikeClick(btn.count--)
-                            alert("You liked this video")
-                            // SetLikeClick(!likeClick)
-                            // btn.clicked = likeClick
-                        } else if (btn.idx == 1) {
-                            btn.clicked = !btn.clicked
-                            btn.clicked ? SetMsgClick(btn.count++) : SetMsgClick(btn.count--)
-                            SetMsgClick(!msgClick)
-                            btn.clicked = likeClick
-                            alert("You messaged this video")
-                        } else if (btn.idx == 2) {
-                            btn.clicked = !btn.clicked
-                            btn.clicked ? SetBookmarkClick(btn.count++) : SetBookmarkClick(btn.count--)
-                            alert("You bookmarked this video")
-                            // SetBookmarkClick(!bookmarkClick)
-                            // btn.clicked = likeClick
-                        } else {
-                            btn.clicked = !btn.clicked
-                            btn.clicked ? SetShareClick(btn.count++) : SetShareClick(btn.count--)
-                            alert("You shared this video")
-                            // SetShareClick(!shareClick)
-                            // btn.clicked = likeClick
-                        }
-                        btn.clicked == true ? btn.count++ : btn.count--
-                        // btn.clicked = !btn.clicked
-                        // console.log(btn.clicked)
-                        // if (btn.clicked == true) {
-                        //     btn.count++
-                        // } else {
-                        //     btn.count--
-                        // }
-                        console.log(idx, btn.count)
-                    }}>
-                        {btn.icon}
-                        <Typography color='white'>{btn.count}</Typography>
-                    </Button>
+        <Stack direction={'row'} width={'90vw'} >
+            <Stack direction='row' width={'80vw'} gap={2} sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Video />
 
-                ))}
-                {/* <Button startIcon={<FavoriteIcon color='white' />} width={'200px'} height={'200px'} sx={{ bgcolor: 'gray', color: 'white' }} />
+
+                <Stack direction={'column'} justifyContent={'end'} mb={1} >
+                    <Box mb={2} ml={2.68} sx={{ display: 'flex', 'flex-direction': 'column', justifyContent: 'center', height: '40px', width: '40px', transform: 'scale(1.5)' }}>
+                        <UserButton />
+                    </Box>
+                    {action.map((btn, idx) => (
+                        <Button key={idx} width={'10px'} height={'200px'} sx={{ display: 'flex', 'flex-direction': 'column', justifyContent: 'center' }} onClick={() => {
+                            if (btn.idx == 0) {
+                                btn.clicked = !btn.clicked
+                                btn.clicked ? SetLikeClick(btn.count++) : SetLikeClick(btn.count--)
+                                alert("You liked this video")
+                                // SetLikeClick(!likeClick)
+                                // btn.clicked = likeClick
+                            } else if (btn.idx == 1) {
+                                btn.clicked = !btn.clicked
+                                btn.clicked ? SetMsgClick(btn.count++) : SetMsgClick(btn.count--)
+                                SetMsgClick(!msgClick)
+                                btn.clicked = likeClick
+                                alert("You messaged this video")
+                            } else if (btn.idx == 2) {
+                                btn.clicked = !btn.clicked
+                                btn.clicked ? SetBookmarkClick(btn.count++) : SetBookmarkClick(btn.count--)
+                                alert("You bookmarked this video")
+                                // SetBookmarkClick(!bookmarkClick)
+                                // btn.clicked = likeClick
+                            } else {
+                                btn.clicked = !btn.clicked
+                                btn.clicked ? SetShareClick(btn.count++) : SetShareClick(btn.count--)
+                                alert("You shared this video")
+                                // SetShareClick(!shareClick)
+                                // btn.clicked = likeClick
+                            }
+                            btn.clicked == true ? btn.count++ : btn.count--
+                            // btn.clicked = !btn.clicked
+                            // console.log(btn.clicked)
+                            // if (btn.clicked == true) {
+                            //     btn.count++
+                            // } else {
+                            //     btn.count--
+                            // }
+                            console.log(idx, btn.count)
+                        }}>
+                            {btn.icon}
+                            <Typography color='white'>{btn.count}</Typography>
+                        </Button>
+
+                    ))}
+                    {/* <Button startIcon={<FavoriteIcon color='white' />} width={'200px'} height={'200px'} sx={{ bgcolor: 'gray', color: 'white' }} />
                 <Button startIcon={<SmsIcon sx={{ transform: 'scaleX(-1)' }} />} sx={{ bgcolor: 'gray', color: 'white' }} />
                 <Button startIcon={<BookmarkIcon />} sx={{ bgcolor: 'gray', color: 'white' }} />
                 <Button startIcon={<ReplyIcon sx={{ transform: 'scaleX(-1)' }} />} sx={{ bgcolor: 'gray', color: 'white' }} /> */}
-
+                </Stack>
             </Stack>
-            {/* <img src='../group-pic.png' /> */}
+            <Stack direction={'column'} justifyContent={'center'} gap={2}>
+                <Button>
+                    <ArrowUpwardIcon fontSize='large' sx={{ bgcolor: grey[900], color: 'white', borderRadius: '20px', height: '40px', width: '40px', p: 1 }} />
+                </Button>
+                <Button>
+                    <ArrowDownwardIcon fontSize='large' sx={{ bgcolor: grey[900], color: 'white', borderRadius: '20px', height: '40px', width: '40px', p: 1 }} />
+                </Button>
+            </Stack>
+
         </Stack>
     )
 }
